b36b9d6be924f3e3bf018becddf26c12411e0027
DinisCruz <dinis.cruz@owasp.org>
2012-10-22 03:38:35 +0100

    added MS Ajax Javascript file and made test TM Webservice call

--- TM/TM_Mobile.js
+++ TM/TM_Mobile.js
@@ -1,16 +1,23 @@
 ﻿$(function()
 	{	
 		populateHomePageList();	
+
+	SecurityInnovation.TeamMentor.WebClient.WebServices.TM_WebServices.Login_PwdInClearText("admin","!!tmadmin",alert);
 	});
+
 
 var populateHomePageList = function()
 	{
 		$("#homepage_list").append('<li>Available Libraries</li>');
 		
-		var libraryName = "OWASP";
-		var liHtml = '<li><a href="#'+ /*libraryName*/ '' +'about"		data-transition="slide"><img src="/TM/SingleLibrary.png" alt="'+libraryName +
-					 '" class="ui-li-icon">'+ libraryName +'</a></li>';
-		$("#homepage_list").append(liHtml);
+		var addLibraryLink = function(libraryName)
+			{		
+				var liHtml = '<li><a href="#'+ /*libraryName*/ '' +'about"		data-transition="slide"><img src="/TM/SingleLibrary.png" alt="'+libraryName +
+							 '" class="ui-li-icon">'+ libraryName +'</a></li>';
+				$("#homepage_list").append(liHtml);
+			};
 
+		addLibraryLink("OWASP");
+		addLibraryLink(".Net 3.5");
 		$("#homepage_list").listview('refresh');
 	};
