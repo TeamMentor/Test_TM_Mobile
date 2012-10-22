815003d6d041b63a4f523d8ff027850472e8da5b
DinisCruz <dinis.cruz@owasp.org>
2012-10-22 03:26:22 +0100

    First set of dynamically created HomePage ListView Items, Added TM Library image

--- TM/TM_Mobile.js
+++ TM/TM_Mobile.js
@@ -1,4 +1,16 @@
 ﻿$(function()
-	{
-	//	alert($("#homepage_list").html());
+	{	
+		populateHomePageList();	
 	});
+
+var populateHomePageList = function()
+	{
+		$("#homepage_list").append('<li>Available Libraries</li>');
+		
+		var libraryName = "OWASP";
+		var liHtml = '<li><a href="#'+ /*libraryName*/ '' +'about"		data-transition="slide"><img src="/TM/SingleLibrary.png" alt="'+libraryName +
+					 '" class="ui-li-icon">'+ libraryName +'</a></li>';
+		$("#homepage_list").append(liHtml);
+
+		$("#homepage_list").listview('refresh');
+	};
